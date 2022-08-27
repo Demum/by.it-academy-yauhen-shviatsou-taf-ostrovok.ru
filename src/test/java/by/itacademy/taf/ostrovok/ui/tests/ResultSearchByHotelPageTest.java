17821937034b9be7e6ca849aee92871217e19055
Yauhen Shviatsou <demum1930@gmail.com>
2022-08-27 20:17:12 +0300

    feat:steps finished

--- src/test/java/by/itacademy/taf/ostrovok/ui/tests/ResultSearchByHotelPageTest.java
+++ src/test/java/by/itacademy/taf/ostrovok/ui/tests/ResultSearchByHotelPageTest.java
@@ -1,7 +1,7 @@
 package by.itacademy.taf.ostrovok.ui.tests;
 
-import by.itacademy.taf.ostrovok.ui.pageobjects.MainPage;
 import by.itacademy.taf.ostrovok.ui.pageobjects.ResultSearchByHotelPage;
+import by.itacademy.taf.ostrovok.ui.steps.ResultSearchByHotelPageSteps;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
@@ -9,22 +9,9 @@ public class ResultSearchByHotelPageTest extends BaseTest {
 
     @Test
     public void testResultSearchByHotelPage(){
-        MainPage mainPage = new MainPage();
+       ResultSearchByHotelPageSteps resultSearchByHotelPageSteps = new ResultSearchByHotelPageSteps();
+       resultSearchByHotelPageSteps.searchByHotel();
         ResultSearchByHotelPage resultSearchByHotelPage = new ResultSearchByHotelPage();
-        mainPage.setInputDestinationSting(resultSearchByHotelPage.getInputHotelName());
-
-        mainPage.openPage()
-                .typeDestination(mainPage.getInputDestinationSting())
-                .clickSugestDestination()
-                .clickButtonCheckInDatePicker()
-                .clickDatePickerCheckIn()
-                .clickDatePickerCheckOut()
-                .clickButtonGuestInput()
-                .clickButtonAdultMinus()
-                .clickButtonAddChild()
-                .clickButtonAddChildTwoYearsOld()
-                .clickButtonDoneWithChild()
-                .clickButtonSearch();
-        Assert.assertEquals(resultSearchByHotelPage.getNameOfTheOpenedHotel(), mainPage.getInputDestinationSting(), "Opened Hotel does not match with inputNameHotel");
+        Assert.assertEquals(resultSearchByHotelPage.getNameOfTheOpenedHotel(), resultSearchByHotelPage.getInputHotelName(), "Opened Hotel does not match with inputNameHotel");
     }
 }
