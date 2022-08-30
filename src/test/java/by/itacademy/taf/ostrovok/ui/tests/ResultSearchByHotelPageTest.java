4e060e2ec88e7352d30ca48e90f7978c4b1959e8
Yauhen Shviatsou <demum1930@gmail.com>
2022-08-30 22:56:04 +0300

    code refactor

--- src/test/java/by/itacademy/taf/ostrovok/ui/tests/ResultSearchByHotelPageTest.java
+++ src/test/java/by/itacademy/taf/ostrovok/ui/tests/ResultSearchByHotelPageTest.java
@@ -13,6 +13,6 @@ public class ResultSearchByHotelPageTest extends BaseTest {
         resultSearchByHotelPageSteps.searchByHotel();
         ResultSearchByHotelPage resultSearchByHotelPage = new ResultSearchByHotelPage();
         Assert.assertEquals(resultSearchByHotelPage.getNameOfTheOpenedHotel(), resultSearchByHotelPage.getInputHotelName(),
-                "Opened Hotel does not match with inputNameHotel");
+                "Opened Hotel "+resultSearchByHotelPage.getNameOfTheOpenedHotel()+ "does not match with inputNameHotel:"+resultSearchByHotelPage.getInputHotelName());
     }
 }
